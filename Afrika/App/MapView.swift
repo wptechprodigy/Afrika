247108dffe8fbdcb559575bef1e2538907277adf
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-06-15 20:30:12 +0100

    organise the project structure with dummy tab contents

---
+++ Afrika/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Afrika
+//
+//  Created by waheedCodes on 15/06/2021.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ Afrika/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Afrika
+//
+//  Created by waheedCodes on 15/06/2021.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
