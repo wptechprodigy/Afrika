04c3200aa268af03af7e229d38a79066622819dc
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-06-15 23:34:22 +0100

    add animal model

---
+++ Afrika/Models/Animal.swift
@@ -0,0 +1,19 @@
+//
+//  Animal.swift
+//  Afrika
+//
+//  Created by waheedCodes on 15/06/2021.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
