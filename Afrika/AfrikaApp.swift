54dd0c27f2255a51e8215937cecfe9aea101a135
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-06-15 18:28:20 +0100

    Initial Commit

---
+++ Afrika/AfrikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfrikaApp.swift
+//  Afrika
+//
+//  Created by waheedCodes on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfrikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Afrika/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Afrika
+//
+//  Created by waheedCodes on 15/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
