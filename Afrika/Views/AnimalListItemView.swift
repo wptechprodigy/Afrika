7ecc563f3a0933a45ed7b6628ef1eb550c0ad344
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-06-15 23:45:22 +0100

    populate animal list item view with dynamic ocntent

--- Afrika/Views/AnimalListItemView.swift
+++ Afrika/Views/AnimalListItemView.swift
@@ -10,24 +10,26 @@ import SwiftUI
 struct AnimalListItemView: View {
     // MARK: - PROPERTIES
     
+    let animal: Animal
+    
     
     // MARK: - BODY
     
     var body: some View {
         HStack(alignment: .center, spacing: 16, content: {
-            Image("lion")
+            Image(animal.image)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 90, height: 90)
                 .clipShape(RoundedRectangle(cornerRadius: 12))
             
             VStack (alignment: .leading, spacing: 8, content: {
-                Text("Lion")
+                Text(animal.name)
                     .font(.title2)
                     .fontWeight(.heavy)
                     .foregroundColor(.accentColor)
                 
-                Text("The world's most social felines; lions roam the savannas and grasslands of the African Continent, hunting cooperatively, and raising cubs in prides.")
+                Text(animal.headline)
                     .font(.footnote)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
@@ -41,8 +43,10 @@ struct AnimalListItemView: View {
 // MARK: - PREVIEW
 
 struct AnimalListItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     static var previews: some View {
-        AnimalListItemView()
+        AnimalListItemView(animal: animals[1])
             .previewLayout(.sizeThatFits)
             .padding()
     }
