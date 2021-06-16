7b91f32a3ea1b263738d36e28e0da2193206cbd5
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-06-16 19:10:52 +0100

    implement the video list

--- Afrika/App/VideoListView.swift
+++ Afrika/App/VideoListView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { video in
+                    VideoListItemView(video: video)
+                        .padding(.vertical, 8)
+                } // : - LOOP
+            } // : - LIST
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .listStyle(InsetGroupedListStyle())
+        } // : - NAV
     }
 }
+
+// MARK: - PREVIEW
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
