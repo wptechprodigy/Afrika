f40d98ce381128ebcab8887c5e55dda42e25c652
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-06-16 18:27:36 +0100

    add a video list item

---
+++ Afrika/Views/VideoListItemView.swift
@@ -0,0 +1,59 @@
+//
+//  VideoListItemView.swift
+//  Afrika
+//
+//  Created by waheedCodes on 16/06/2021.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    // MARK: - PROPERTIES
+    
+    let video: Video
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack (spacing: 10) {
+            ZStack {
+                Image("video-\(video.id)")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } // : - ZSTACK
+            
+            VStack (alignment: .leading, spacing: 10, content: {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .multilineTextAlignment(.leading)
+                    .font(.footnote)
+                    .lineLimit(2)
+            }) // : - VSTACK
+        } // : - HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+struct VideoListItemView_Previews: PreviewProvider {
+    static let videos: [Video] = Bundle.main.decode("videos.json")
+    
+    static var previews: some View {
+        VideoListItemView(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
